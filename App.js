853d2234a221dee7ee74397e61bf2394b6ac996d
Amir 0d <amirmsut@gmail.com>
2023-09-15 02:13:29 +0330

    install prebuild for install android,iOS

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import { SafeAreaView, StyleSheet } from "react-native";
 import Signin from "./src/screens/auth/Signin/Signin";
+import { useEffect } from "react";
 // import Splash from "./src/screens/auth/Splash/Splash";
 
+const WEB_CLIENT_ID =
+    "286125874361-mjcgbtn5v0p0iur07s3mvlfe9n4dqad4.apps.googleusercontent.com";
+const IOS_CLIENT_ID =
+    "286125874361-l0k696tlla80308vib3gbvpc1c7npov9.apps.googleusercontent.com";
+
 export default function App() {
+    //
+    useEffect(() => {}, []);
+    //
     return (
         <SafeAreaView>
             {/* <Splash /> */}
