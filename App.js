6cf37562eeee51678674e3939adeb6af552cfe0b
Amir 0d <amirmsut@gmail.com>
2023-09-15 02:38:23 +0330

    Manage navigation theme

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
 import { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -7,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Signin from "./src/screens/auth/Signin/Signin";
 import Splash from "./src/screens/auth/Splash/Splash";
 import Signup from "./src/screens/auth/Signup/Signup";
+import { colors } from "./src/utils/colors";
 
 const WEB_CLIENT_ID =
     "286125874361-mjcgbtn5v0p0iur07s3mvlfe9n4dqad4.apps.googleusercontent.com";
@@ -16,23 +16,21 @@ const REVERSE_IOS_CLIENT_ID =
     "com.googleusercontent.apps.286125874361-l0k696tlla80308vib3gbvpc1c7npov9";
 
 export default function App() {
-    //
-    useEffect(() => {}, []);
-    //
-
-    // const Stack = createNativeStackNavigator();
+    const theme = {
+        colors: {
+            background: colors.white,
+        },
+    };
 
     const Stack = createNativeStackNavigator();
     return (
-        // <SafeAreaView>
-        <NavigationContainer>
+        <NavigationContainer theme={theme}>
             <Stack.Navigator>
                 <Stack.Screen name="Splash" component={Splash} />
                 <Stack.Screen name="Signin" component={Signin} />
                 <Stack.Screen name="Signup" component={Signup} />
             </Stack.Navigator>
         </NavigationContainer>
-        // </SafeAreaView>
     );
 }
 
