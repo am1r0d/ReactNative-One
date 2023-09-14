e15a64906bf852c2f0d5660352b6943722e2bc5b
Amir 0d <amirmsut@gmail.com>
2023-09-14 19:01:07 +0330

    Building the Splash Screen

---
+++ src/screens/auth/Splash/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: { padding: 23 },
+    image: {
+        width: "100%",
+        height: 200,
+    },
+    title: { fontSize: 40, fontWeight: "bold", textAlign: "center" },
+    innerTitle: { color: "#FCA340", textDecorationLine: "underline" },
+});
